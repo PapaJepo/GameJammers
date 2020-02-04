827688de72674aea40a3b7180d821abddb701a41
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2020-02-04 12:55:13 +0000

    1

---
+++ GameJammers/Assets/Carry.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carry : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    GameObject pickedUp;
+    float holdDistance;
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 100, Color.red);
+        if(Input.GetMouseButton(0))
+        {
+            RaycastHit raycastHit;
+            
+            if(Physics.Raycast(transform.position,transform.forward,out raycastHit))
+            {
+                if(raycastHit.collider.gameObject.tag == "Carry")
+                {
+                    Debug.Log("RAYCASTHIT");
+                    pickedUp = raycastHit.collider.gameObject;
+                    holdDistance = Vector3.Distance(pickedUp.transform.position, transform.position);
+                }
+                else
+                {
+                    pickedUp = null;
+                }
+            }
+            else
+            {
+                Vector3 holdPos = transform.position + (transform.forward * holdDistance);
+                Vector3 toHoldPos = holdPos - pickedUp.transform.position;
+                pickedUp.GetComponent<Rigidbody>().velocity = toHoldPos;
+            }
+
+        }
+
+
+    }
+}
---
+++ GameJammers/Assets/Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Vector2 i_movement;
+    float moveSpeed = 4f;
+
+    public string Horizontal = "Horizontal_P1";
+    public string Vertical = "Vertical_P1";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        float x = Input.GetAxisRaw(Horizontal);
+        float z = Input.GetAxisRaw(Vertical);
+        Debug.Log(x);
+
+        Vector3 m = new Vector3(x, 0, z) * moveSpeed * Time.deltaTime;
+       // transform.rotation = Quaternion.Slerp(transform.rotation,Quaternion.LookRotation(m),0.05f);
+        transform.Translate(m, Space.World);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(m), 0.05f);
+
+        while(x == 0 && z == 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(m), 0.05f);
+        }
+
+        
+    }
+
+
+ 
+}
